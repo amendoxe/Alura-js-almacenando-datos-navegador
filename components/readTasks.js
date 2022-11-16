5c5294efeb05dc974c96464ba583a5d63a20a4ef
amendoxe <arturo_mendoza@outlook.com>
2022-11-16 11:01:20 -0600

    04.3 algo

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,4 +1,5 @@
 import { createTask } from "./addTask.js";
+import { uniqueDates } from "../services/date.js";
 import dateElement from "./dateElement.js";
 
 export const readTasks = () => {
@@ -6,6 +7,13 @@ export const readTasks = () => {
 	console.log(list);
 
 	const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+	const dates = uniqueDates(taskList);
+	console.log(dates);
+
+	dates.forEach((date) => {
+		console.log(date);
+	});
+
 	taskList.forEach((task) => {
 		list.appendChild(dateElement(task.dateFormat));
 		list.appendChild(createTask(task));
